dc814c75772cb0ace978a473216354511385242b
Grace Dmello <77654364+sea-shanties@users.noreply.github.com>
2024-02-18 19:21:56 -0800

    Switch to Gemini

--- api/server.js
+++ api/server.js
@@ -4,21 +4,13 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const OpenAIApi = require("openai");
 const { parse } = require('dotenv');
 
 let dotenv = require('dotenv').config();
 
-// const openAIConfig = new Configuration()
-const openai = new OpenAIApi({
-    organization: process.env.OPENAI_ORGANIZATION,
-    apiKey: process.env.OPENAI_API_KEY
-});
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEN_AI_API_KEY);
 
 async function getAIWeatherQuote(temp) {
     if (typeof temp != "string") return false;
@@ -27,20 +19,23 @@ async function getAIWeatherQuote(temp) {
     if (isNaN(temp_int)) return false;
     if (temp_int < -128 || temp_int > 134) return false;
 
-    const completion = await openai.chat.completions.create({
-        messages: [{ role: 'user', content: 'You are a weather quip bot. The current temperature is ' + temp + ' degrees fahrenheit. Provide a fun witty comment about the current weather. Keep the comment short.' }],
-        model: 'gpt-3.5-turbo',
-    })
-    return completion.choices[0].message.content;
-}
 
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const prompt = 'The current temperature is ' + temp + ' degrees fahrenheit. Provide a fun, witty comment about the current weather.'
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+
+    return text;
+}
 
 app.get('/quote', async (req, res) => {
     res.send(await getAIWeatherQuote(req.query.temp));
 })
 
 app.get('/test', async (req, res) => {
-    res.send(req.query.temp);
+    res.send("Success");
 })
 
 app.listen(port, () => {
